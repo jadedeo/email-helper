0d06ee2f0a785b276991ffca71b51b9fb2c3d183
jadedeo <jadedeo@gmail.com>
2025-07-25 17:21:08 -0400

    remove old window files

--- src/pages/generate-email/generate-email.js
+++
@@ -1,7 +0,0 @@
-// src/windows/generate-email/generate-email.js
-
-// import { createApp } from "vue";
-// import GenerateEmail from "./GenerateEmail.vue";
-
-// const app = createApp(GenerateEmail);
-// app.mount("#app");
